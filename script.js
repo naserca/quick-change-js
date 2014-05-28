edf42c17bd81e009919bc4844106728cb7ccb452
Alex Naser <c.a.naser@gmail.com>
2014-05-28 01:02:17 -0400

    saving on contentEditable blur

--- script.js
+++ script.js
@@ -5,38 +5,35 @@ var Content = Parse.Object.extend("Content");
 var $contentElems = $('[data-cms]');
 
 $contentElems.each(function(i) {
-  $elem = $(this);
+  var $elem = $(this);
+  $elem.attr('contentEditable', true);
+  $elem.css('display', 'none');
 
   var contentId = $elem.data('cms');
 
-  var query   = new Parse.Query(Content);
+  var query = new Parse.Query(Content);
   query.equalTo("contentId", contentId);
 
-  query.first({
-    success: function(savedContent) {
-      if (!!savedContent) {
-        // load already-saved content into appropriate place
-        $elem.html(savedContent.html);
-      } else {
-        // first instance of the content. save it to the DB
-        var content = new Content();
-        content.save({
-          contentId: contentId,
-          html: $elem.html()
-        }, {
-          success: function(savedContent) {
-            console.log(savedContent);
-          },
-          error: function(model, err) {
-            console.log(err);
-          }
-        });
-      }
-    },
-    error: function(err) {
-      console.log(err);
+  query.first().then(function(savedContent) {
+    if (!!savedContent) {
+      // load already-saved content into appropriate place
+      $elem.html(savedContent.get('html'));
+      $elem.css('display', '');
+    } else {
+      // first instance of the content. save it to the DB
+      var content = new Content();
+      content.save({
+        contentId: contentId,
+        html: $elem.html()
+      });
     }
   });
-});
 
-$contentElems.
+  $elem.blur(function(ev) {
+    query.first().then(function(savedContent) {
+      savedContent.save({
+        html: $elem.html()
+      });
+    });
+  });
+});
